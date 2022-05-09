1eefb8e4d5800440b334b8a16080d1ff97ff396b
Nickolas Hughes <6713071+whimzyy@users.noreply.github.com>
2022-05-09 02:33:18 -0600

    Font and Button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 					<div className="front-para">
 						<p>
 							There are so many moving pieces and new additions to the DeFi<br /> 
-							and Crypto spaces everyday. The <mark className='grad'>LIQUID</mark> suite of DeFi tools <br /> 
+							and Crypto spaces everyday. The<mark className='grad'>LIQUID</mark>suite of DeFi tools <br /> 
 							will help you stay ahead of the trend and grow your portfolio.
 						</p>
 					</div>
